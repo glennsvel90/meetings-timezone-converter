1fad0c838f5b79f625a8134256fe8a78394cd799
Glenn Velupillai <glennselwynvel@gmail.com>
2018-05-01 22:11:31 -0400

    pep 257

--- meetings.py
+++ meetings.py
@@ -15,11 +15,11 @@ fmt = '%a %b %d  %I:%M %p %Z%z'
 while True:
     date_input = input("When is your meeting? Please use MM/DD/YYYY HH:MM am/pm format. ")
     try:
-        #try turning a string to a datetime
+        # try turning a string to a datetime
         local_date = datetime.datetime.strptime(date_input, '%m/%d/%Y %I:%M %p')
     except ValueError:
         print("{} doesn't seem to be a valid date & time.".format(date_input))
-    #if "try" runs smoothly, then after that, the "else" happens.
+    # if "try" runs smoothly, then after that, the "else" happens.
     else:
         local_date = pytz.timezone('US/Eastern').localize(local_date)
         utc_date = local_date.astimezone(pytz.utc)
